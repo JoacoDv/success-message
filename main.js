a06e0ff7195275d7a2ec2d4f07fc6853e8c97431
unknown <joacodv2003@gmail.com>
2025-03-13 13:50:10 -0300

    add animations

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
 const main = document.getElementById("main")
-const form = document.getElementById("form")
 const img = document.getElementById("img")
+const title = document.querySelector(".header")
+const list = document.querySelector("ul")
 const email = document.getElementById("email")
 const container = document.getElementById("container")
-const card = document.querySelector(".success")
 const submitBtn = document.getElementById("submitBtn")
-const dissmisBtn = document.getElementById("dissmisBtn")
+const card = document.querySelector(".success")
+const cardTextContainer = document.querySelector(".success-icon-and-text")
+const dissmisBtn = document.getElementById("dismissbtn")
 
-if(window.innerWidth <= 800) {
-    main.insertBefore(img, form)
+import { animate, scroll } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm"
+
+
+// ANIMATIONS
+function start() {
+    let delayLi = 1.1
+    animate(main, {y: [-30, 0] ,opacity: [0,1], scale: [.5,1]}, {ease: "easeOut", duration: 0.5})
+    animate(img, {scale: [.4,1], opacity: [0,1]}, {ease:"easeOut", duration: .5, delay: 0.5})
+    animate(title, {x: [30, 0], opacity:[0,1]}, {ease: "easeOut", duration: .3, delay: .8})
+    Array.from(list.children).forEach(li => {
+        animate(li, {y: [-60, 0], opacity: [0,1]}, {ease: "easeOut", duration: .2, delay: delayLi})
+        delayLi += .2
+    })
+    animate(container, {x: [30, 0], opacity: [0,1]}, {ease: "easeOut", duration: .3, delay: 1.7})
+    animate(submitBtn, {opacity: [0,1], scale: [.5,1]}, {ease: "easeOut", duration: .3, delay: 2})
 }
 
-const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+function changeCard() {
+    animate(card, {y: [-30, 0] ,opacity: [0,1], scale: [.5,1]}, {ease: "easeOut", duration: 0.5})
+    animate(cardTextContainer, {y: [-30, 0] ,opacity: [0,1]}, {ease: "easeOut", duration: 0.5, delay: .5})
+    animate(dissmisBtn, {y: [30, 0] ,opacity: [0,1], scale: [.6,1]}, {ease: "easeOut", duration: 0.5, delay: 1})
+}
 
+start()
+
+// FUNCIONAMIENTO
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 const verificarEmail = () => {
     if(emailRegex.test(email.value)) {
@@ -27,12 +50,17 @@ const verificarEmail = () => {
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault()
     verificarEmail()
+    changeCard()
 })
 
 
 dissmisBtn.addEventListener("click", () => {
     main.style.display = "flex" 
-        card.style.display = "none" 
+    card.style.display = "none" 
+    if (email.classList.contains("error")) { 
         email.classList.toggle("error")
         container.classList.toggle("container")
+    }
+    email.value = ""
+    start()
 })
